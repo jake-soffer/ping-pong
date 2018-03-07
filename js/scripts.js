50662e246645c38b90873103db6201d32b3c07a4
jbs <soffer94@gmail.com>
2018-03-07 12:38:24 -0800

    Configured array functionality in business/ui logic.

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,18 @@ var divide = function(number) {
   var resultArray = [];
   for (var i = 1; i <= number; i += 1)
     resultArray.push(i);
-  if (i === 3) {
-    resultArray.push("ping");
+  return resultArray;
   }
+
+  // if (i % 3 === 0) {
+  //   resultArray.push("ping");
+  // }
   // } else if (i % 5 === 0 && i % 3 !== 0) {
   //   resultArray.push("pong");
   // } else if (i % 5 === 0 && i % 3 === 0) {
   //   resultArray.push("pingpong");
   // }
-};
+//};
 
 // UI LOGIC
 
