0c6ed798fd513eaea7d459260ada4b7fb0215be9
jbs <soffer94@gmail.com>
2018-03-07 20:56:12 -0800

    Add min value to number input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 // BUSINESS LOGIC
-var divide = function(number) {
-var resultArray = [];
-for (var i = 1; i <= number; i += 1)
+var pongMachine = function(number) {
 
-if ((i % 3 === 0) && (i % 5 !== 0)) {
-resultArray.push("ping");
-} else if ((i % 3 !== 0) && (i % 5 === 0)) {
-resultArray.push("pong");
-} else if (i % 15 === 0) {
-resultArray.push("pingpong");
-} else {
-resultArray.push(i);
-}
-
-return resultArray;
+  var resultArray = [];
+  for (var i = 1; i <= number; i += 1) {
+    if ((i % 3 === 0) && (i % 5 !== 0)) {
+      resultArray.push("ping");
+    } else if ((i % 3 !== 0) && (i % 5 === 0)) {
+      resultArray.push("pong");
+    } else if (i % 15 === 0) {
+      resultArray.push("pingpong");
+    } else {
+      resultArray.push(i);
+    }
+  }
+  return resultArray;
 };
 
 // UI LOGIC
@@ -22,9 +22,9 @@ $(document).ready(function() {
   $("form#number").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("input#number").val());
-    var resultArray = divide(number);
+    var resultArray = pongMachine(number);
 
-    $(".answer").text(resultArray)
-
+    $(".results").show();
+    $(".answer").text(resultArray);
   });
 });
